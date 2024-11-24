cf56055a40f60089e77d39d0187ab0dc0101bfd4
mihaelaaa-23 <mihaela.untu@gmail.com>
2024-11-24 20:46:47 +0200

    Task 5: Implemented the Scheduler class and its time managing functionalities

---
+++ lab-papers-please/java-classifcation/src/main/java/oop/practice/lab3/task5/Main.java
@@ -0,0 +1,15 @@
+package oop.practice.lab3.task5;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scheduler scheduler = new Scheduler("src/main/java/oop/practice/lab3/queue");
+        scheduler.schedulePolling(3); // Poll every 3 seconds
+        scheduler.scheduleServing(5); // Serve every 5 seconds
+
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("\nShutting down program...");
+        }));
+    }
+}
